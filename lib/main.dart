999a2c6a0bf3da7ecdc160cfa0afa3b7b90f7e11
Jose Vegini <josevegini12@gmail.com>
2022-08-17 22:11:27 -0300

    Codigo Atualizado final aula 03

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.teal,
-      body: Container(),
+  runApp( const BusinessCard(),
     ),
    )
+
+
+        }
+      }
+
   );
+
+  class BusinessCard extends StatelessWidget {
+    const BusinessCard({Key? key}) : super(key: key);
+
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+          home: Scaffold(
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+            child: Container(
+              child: Text("Hello World!"),
+              color: Colors.amber,
+              width: 100.0,
+              height: 100.0,
+              margin: EdgeInsets.all(30.0),
+      ),
+          ),
+    }
+  }
+
 }
 
